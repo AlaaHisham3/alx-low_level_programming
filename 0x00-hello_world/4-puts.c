276dccc2b5d4ef1de45e0d7ef786cb62c546f362
root <Alaa_Hisham>
2022-10-15 05:43:07 -0700

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 
-
 /**
+ * main - print a text using puts function
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
- puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
