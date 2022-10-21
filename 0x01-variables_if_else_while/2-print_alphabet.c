17078215cb5ef09015ce302ec43462bc56a85cd8
root <Alaa_Hisham>
2022-10-21 14:35:43 -0700

    new commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,17 +4,21 @@
 
 /* more headers goes here */
 /**
-* main - if else statment
+* main - print_alphabet
 *
 * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
+char c;
+c = 'a';
+while (c <= 'z')
+{
+	putchar(c);
+	c = c + 1;
+}
 
+putchar('\n');
 return (0);
 }
