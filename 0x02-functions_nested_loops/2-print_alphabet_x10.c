a49bf559c030735e061d372050bcda2202166af4
root <Alaa_Hisham>
2022-10-22 12:44:42 -0700

    new cvommit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	char y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+}
