0209cb2adb306e726952bf99cbf2d804c05bb577
root <Alaa_Hisham>
2022-10-22 15:33:01 -0700

    new commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - a function that prints the alphabet, in lowercase
+ * @n: The character to be checked.
+ *
+ * Return: Always 0.
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar(n + '0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
