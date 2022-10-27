39841d5193cd82012471fe5f98de302efa85cb30
root <Alaa_Hisham>
2022-10-27 06:19:37 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that prints the alphabet, in lowercase
+ * @n : pointer
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
