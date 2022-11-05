0a5b0f28e00ce19d768b0aba0847ab64909cadf6
root <Alaa_Hisham>
2022-11-04 23:00:46 -0700

    new commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: counter
+ * @argv: array
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
