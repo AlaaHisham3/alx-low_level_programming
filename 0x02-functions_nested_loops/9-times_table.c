aca9c65dcca9653c7d7150f00019876358447106
root <Alaa_Hisham>
2022-11-14 00:46:05 -0800

    new commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * times_table - a function that prints the alphabet, in lowercaset
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int h, m, x;
+
+	for (h = 0; h < 10; h++)
+	{
+		for (m = 0; m < 10; m++)
+		{
+			x = h * m;
+			if (m != 9)
+			{
+			if (x < 10)
+			{
+			_putchar(' ');
+			_putchar((x % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			}
+			else
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			}
+			else
+			{
+				if (x < 10)
+				{
+					_putchar(' ');
+					_putchar((x % 10) + '0');
+				}
+				else
+				{
+					_putchar((x / 10) + '0');
+					_putchar((x % 10) + '0');
+				}
+			}
+		}
+			_putchar('\n');
+	}
+}
