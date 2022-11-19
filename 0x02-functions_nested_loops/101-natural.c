1effdb2e015021e9131a51cb03d50d29022d4400
root <Alaa_Hisham>
2022-11-19 07:08:12 -0800

    new commit

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,33 +3,33 @@
 
 /**
  * natural_numbers - a function that prints the alphabet, in lowercaset
- * 
+ *
  * Return: Always 0.
  */
+void natural_numbers(void);
 void natural_numbers(void)
 {
 	int i, sum;
 
 	sum = 0;
 
-	for(i = 3; i < 1024; i++)
+	for (i = 0; i < 1024; i++)
 	{
-		if(i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
-		sum = sum + i;
+			sum = sum + i;
 		}
 	}
-	printf("%d\n", i);
+	printf("%d\n", sum);
 }
 
 /**
  * main - a function that prints the alphabet, in lowercaset
  *
- * return: Always 0
+ * Return: Always 0
  */
-int main (void)
+int main(void)
 {
 	natural_numbers();
 	return (0);
 }
-	
