b25b9f654f39cad88ffd282f9dd63a5d82e1c481
root <Alaa_Hisham>
2022-12-08 04:21:23 -0800

    new commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - check the code
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if (c != 50 && c != 52)
+		{
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
