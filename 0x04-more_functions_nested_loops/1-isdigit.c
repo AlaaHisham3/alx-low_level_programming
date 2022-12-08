4cdd3e5ab6bc4b4495e8fb64ef85c1d9d4629ed5
root <Alaa_Hisham>
2022-12-08 03:37:32 -0800

    new commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check the code.
+ * @c: variable
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
