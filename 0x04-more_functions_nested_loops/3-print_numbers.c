05c3b08e39bdce751949673bb15babf7bf00c47d
root <Alaa_Hisham>
2022-12-08 04:03:29 -0800

    new commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - check the code
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for ( c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
