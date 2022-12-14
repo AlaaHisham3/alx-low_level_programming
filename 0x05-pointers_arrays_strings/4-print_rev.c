55312471d97acb56947593bb14372f7205ac64b3
root <Alaa_Hisham>
2022-12-13 23:49:50 -0800

    new commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints the alphabet, in lowercase
+ * @s: pointer
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int num;
+
+	for (i = 0; s[i] != '\0';)
+	{
+		i = i + 1;
+	}
+	for (num = i; num >= 0; num--)
+	{
+		_putchar(s[num]);
+	}
+	_putchar('\n');
+}
