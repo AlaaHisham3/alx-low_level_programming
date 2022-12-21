cd74ea88f487023e2fa0085f8cfedb1db8701d47
root <Alaa_Hisham>
2022-12-20 23:23:00 -0800

    new commit;

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints the alphabet, in lowercase
+ * @str: pointer
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i +2;
+	}
+	_putchar('\n');
+
+}
